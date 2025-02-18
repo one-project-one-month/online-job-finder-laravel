feacc4fb09e342f77263ebcf6a9145ba219061e6
Moe-Wai-Yan <moorewaiyan@gmail.com>
2025-02-18 22:44:54 +0630

    feature/resume-crud

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\JWTMiddleware;
+use App\Http\Middleware\MustBeApplicant;
 use App\Http\Middleware\CheckAdminMiddleware;
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Middleware\CheckRecruiterMiddleware;
 use App\Http\Controllers\Api\Skills\SkillController;
+use App\Http\Controllers\Api\Resumes\ResumeController;
 use App\Http\Controllers\Api\Locations\LocationController;
+
 use App\Http\Controllers\Api\JobCategory\JobCategoryController;
 use App\Http\Controllers\Api\CompanyProfile\CompanyProfileController;
-use App\Http\Middleware\CheckRecruiterMiddleware;
-
-use App\Http\Controllers\Api\Resumes\ResumeController;
+use App\Http\Controllers\Api\ApplicantProfile\ApplicantProfileController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -25,17 +27,10 @@ Route::middleware([JWTMiddleware::class])->group(function () {
     Route::get('users', [AuthController::class, 'getUser']);
     Route::post('signout', [AuthController::class, 'logout']);
     Route::post('password/change', [AuthController::class, 'changePassword']);
+});
 
-    // Routes that only require JWT token (Public for authenticated users)
-    Route::get('company-profiles', [CompanyProfileController::class, 'index']);
-    Route::get('company-profiles/{id}', [CompanyProfileController::class, 'show']);
-
-    // Routes that require both JWT token & Recruiter role
-    Route::middleware([CheckRecruiterMiddleware::class])->group(function () {
-        Route::post('company-profiles', [CompanyProfileController::class, 'store']);
-        Route::put('company-profiles/{id}', [CompanyProfileController::class, 'update']);
-        Route::delete('company-profiles/{id}', [CompanyProfileController::class, 'destroy']);
-    });
+Route::prefix('recruiter/me/')->middleware([JWTMiddleware::class,MustBeApplicant::class])->group(function(){
+    Route::apiResource('profile',CompanyProfileController::class);
 });
 
 Route::prefix('admin/me')->middleware(JWTMiddleware::class)->group(function (){
@@ -44,10 +39,9 @@ Route::prefix('admin/me')->middleware(JWTMiddleware::class)->group(function (){
     Route::apiResource('locations', LocationController::class)->middleware(CheckAdminMiddleware::class);
 });
 
-
-
-Route::prefix('/me')->middleware([JWTMiddleware::class,MustBeApplicant::class])->group(function(){
+Route::prefix('applicant/me')->middleware([JWTMiddleware::class,MustBeApplicant::class])->group(function(){
     Route::apiResource('profile',ApplicantProfileController::class);
+    Route::apiResource('resumes', ResumeController::class)->middleware(JWTMiddleware::class);
 });
 
-Route::apiResource('resumes', ResumeController::class)->middleware(JWTMiddleware::class);
+
